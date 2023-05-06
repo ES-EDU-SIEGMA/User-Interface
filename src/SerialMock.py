1a20eced4e384b006749b9aeff41c8d5d3586fcd
Serkan Korkmaz <serkan.korkmaz@stud.uni-due.de>
2023-05-06 19:56:26 +0200

    implementing write method

--- src/SerialMock.py
+++ src/SerialMock.py
@@ -28,9 +28,22 @@ class SerialMock:
 			return b''
 			
 	
+	"""
+	Picos are identified by the device they control.
+	Then calibrated comes in the queue
+	so that waitUnitlReady() is properly simulated.
+	Failure case neglected. We create three objects
+	and have three picos for now.
+	"""
 	def write(self, input_data) -> None: 
-		pass
-		
+		if input_data == b'i\n':
+			if "LEFT" in self.port:
+				self.buffer.append(self.responses["LEFT", "CALIBRATED"])				
+			elif "RIGHT" in self.port:
+				self.buffer.append(self.responses["RIGHT", "CALIBRATED"])				
+			elif "RONDELL" in self.port:
+				self.buffer.append(self.responses["RONDELL", "CALIBRATED"])
+					
 	
 	def close(self) -> None:
 		pass
