28b6611834898fd8ab86fbec8830df8ac3e0324a
Markus Imig <markus.imig@stud.uni-due.de>
2023-05-25 11:02:15 +0200

    current progress on data

--- src/libs/data/data_functions.py
+++ src/libs/data/data_functions.py
@@ -1,41 +1,48 @@
-import data_objects
+import data_objects as Data
 
 
 class DataInterface:
-    def __init__(self):
-        pass
+    __runtime_data: Data.RuntimeData  # object to get data from
 
-    # todo check which functions are used
+    def __init__(self):
+        self.__runtime_data = Data.RuntimeData()
+
     def get_beverage_names(self) -> list[str]:
-        pass
+        """ returns a list of all beverage names (beverage names are unique)"""
+        return self.__runtime_data.get_beverage_names()
 
     def get_recipe_names(self) -> list[str]:
-        pass
+        """ returns a list of all recipe names (recipe names are unique)"""
+        return self.__runtime_data.get_recipe_names()
+
+    def get_dispensable_beverage_names(self) -> list[str]:
+        """ returns a list of all recipe names (beverage names are unique)"""
+        return self.__runtime_data.get_dispensable_beverage_names()
+
+    def get_dispensable_recipe_names(self) -> list[str]:
+        """ returns a list of the names of all recipes that can be dispensed (recipe names are unique)"""
+        return self.__runtime_data.get_dispensable_recipe_names()
 
     def get_beverage_hopper_names(self) -> list[str]:
-        pass
-
-    def get_dispensable_drinks(self) -> list[str]:
-        pass
-
-    def get_drink_names(self) -> list[str]:
-        pass
+        """ returns a list of the names of all beverages that can be dispensed (beverage names are unique)"""
+        return self.__runtime_data.get_beverage_hopper_names()
 
     # todo maybe return bool on set cmds
-    def create_recipe(self, __name: str, __name_and_amount: list[str]):
-        pass
+    def create_recipe(self, __name: str, __name_and_amount: str):
+        self.__runtime_data.create_recipe(__name, __name_and_amount)
 
-    # currently set_beverage is not supported
+    # currently create_beverage is not supported
     def create_beverage(self):
         pass
 
     # todo don't know whether this name1,2 thing is good
-    def set_beverage_hopper(self, __name_old, __name_new):
+    def set_beverage_hopper(self, __old_beverage_name: str, __new_beverage_name: str):
         pass
 
-    # idea for this is to create functionality that allows to manipulate already existing drinks
+    # idea for this is to create functionality that allows to manipulate already existing beverages
     def change_beverage(self):
         pass
 
+    # idea for this is to create functionality that allows to manipulate already existing recipes
     def change_recipe(self):
         pass
