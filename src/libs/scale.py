62c3986c8fe5fd90631278543ca4ff1c3ea80fb1
David P. Federl <david-peter.federl@stud.uni-due.de>
2023-05-31 14:29:51 +0200

    fix(scale): fix gpio mock

--- src/libs/scale.py
+++ src/libs/scale.py
@@ -15,7 +15,8 @@ hx = HX711(5, 6)
 def __init__():
     global hx
     global referenceUnit
-    GPIO.setwarnings(GPIO, None)
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
     hx.set_reading_format("MSB", "MSB")
     hx.set_reference_unit(referenceUnit)
     hx.reset()
