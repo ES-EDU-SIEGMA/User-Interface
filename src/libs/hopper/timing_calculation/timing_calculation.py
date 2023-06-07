e06ee674723a148352d24e6834650f3f3ec9f917
Markus Imig <markus.imig@stud.uni-due.de>
2023-06-07 19:43:55 +0200

    create calcultion module

--- src/libs/hopper/timing_calculation/timing_calculation.py
+++ src/libs/hopper/timing_calculation/timing_calculation.py
@@ -1,8 +1,18 @@
 class Calculation:
+    """ assumption_1: the speed for different hopper sizes is the same
+        assumption_2: the required time can be approximated linearly
+        assumption_3: a 40 ml hopper needs 6000 milliseconds
+        -> 6000sec/40ml = 150 milliseconds/ml
+        -> f(x):= x*150*<flow-speed>      where x is the ml-amount of a drink
+        and f(x) the required time it takes to dispense the drink"""
+    __MS_PER_ML = 150
 
     def __init__(self):
         pass
 
     def calculate_timing(self, __data: list[int]):
         # __data:=[<amount-ml>,<flow-speed>];   len(__data)==2;  __data: list[int]
-        pass
+        __amount_ml = __data[0]
+        __flow_speed = __data[1]
+        __timing = __amount_ml * self.__MS_PER_ML * __flow_speed
+        return __timing
