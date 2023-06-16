73753c5e3145e9efe72c3480396cc93738c2afe3
Markus Imig <markus.imig@stud.uni-due.de>
2023-06-16 15:57:02 +0200

    path fix

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import json
+from os.path import abspath as absolute_path, join, dirname, realpath
 
 from libs.business_logic import (
     business_logic as business_logic_module,
@@ -9,14 +10,18 @@ from libs.cli_hardware import cli_hardware as cli_hardware_module
 
 class Main:
     def __init__(self):
+        __configuration_file_path: str = absolute_path(
+            join(dirname(realpath(__file__)), "configuration.json")
+        )
         __configuration_dict: dict
 
         try:
-            with open(file="configuration.json", mode="r") as __configuration_file:
+            with open(file=__configuration_file_path, mode="r") as __configuration_file:
                 __configuration_dict = json.load(__configuration_file)
 
         except Exception as error:
             print(f"can't read in the configuration file. error: {error}")
+            exit(1)
 
         if __configuration_dict["configuration_only_selection"]:
             business_logic_module_only_selection.BusinessLogic(__configuration_dict)
