964e5dede4da3c7cf9858c9a6fd49baeb5dce4a4
David P. Federl <david-peter.federl@stud.uni-due.de>
2023-06-19 10:27:39 +0200

    test(integration-tests) test(scale): add integration for scale

---
+++ src/integration-tests/integration-test-scale.py
@@ -0,0 +1,16 @@
+"""
+simple script to test scale implementation on the hardware
+
+"""
+
+from libs.scale.scale_interface import Scale
+
+__scale: Scale = None
+
+if __name__ == "__main__":
+    __scale = Scale(False, "average", 5)
+
+    for measurement_index in range(1, 10):
+        print(__scale.get_weight())
+
+    __scale.close()
