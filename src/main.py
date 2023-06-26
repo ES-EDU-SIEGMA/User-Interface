217f25ca78723cb508ef31d62c2895529c6c9d1a
Markus Imig <markus.imig@stud.uni-due.de>
2023-06-26 08:43:53 +0200

    remove cli hardware test

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from libs.business_logic import (
     business_logic as business_logic_module,
     business_logic_simple as business_logic_module_only_selection,
 )
-from libs.cli_hardware import cli_hardware as cli_hardware_module
 
 
 class Main:
@@ -40,8 +39,6 @@ class Main:
 
         if __configuration_dict["configuration_only_selection"]:
             business_logic_module_only_selection.BusinessLogic(__configuration_dict)
-        elif __configuration_dict["configuration_cli_hardware"]:
-            cli_hardware_module.CliHardware(__configuration_dict)
         else:
             business_logic_module.BusinessLogic(__configuration_dict)
 
