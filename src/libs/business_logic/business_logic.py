7582b241a53c37a674698d06483a1bfef2db5ecc
Markus Imig <markus.imig@stud.uni-due.de>
2023-06-28 13:29:29 +0200

    comments in code business_logic

--- src/libs/business_logic/business_logic.py
+++ src/libs/business_logic/business_logic.py
@@ -25,12 +25,13 @@ class BusinessLogic:
     __program_state_progress: state_progress_module.StateProgress
 
     __program_state: (
-        state_selection_module.StateSelection
-        | state_edit_module.StateEdit
-        | state_new_module.StateNew
-        | state_progress_module.StateProgress
+            state_selection_module.StateSelection
+            | state_edit_module.StateEdit
+            | state_new_module.StateNew
+            | state_progress_module.StateProgress
     )
 
+    __ui_object: ui_module.UserInterface
     __data_object: data_module.DataInterface
     __scale_object: scale_module.Scale
     __hopper_object: hopper_module.Hopper
@@ -38,7 +39,12 @@ class BusinessLogic:
     __program_is_running: bool
 
     def __init__(self, __configuration: dict):
-        __ui_object: ui_module.UserInterface = ui_module.UserInterface(
+
+        ################################################################################################################
+        # initialize program objects
+        ################################################################################################################
+
+        self.__ui_object = ui_module.UserInterface(
             __configuration["configure_ui_type"]
         )
         self.__data_object = data_module.DataInterface(
@@ -61,45 +67,63 @@ class BusinessLogic:
             __configuration["configure_measurements_per_scale_value"],
         )
 
+        ################################################################################################################
+        # initialize program state objects
+        ################################################################################################################
+
         self.__program_state_selection = state_selection_module.StateSelection(
-            __ui_object, self.__data_object, self.__hopper_object
+            self.__ui_object, self.__data_object, self.__hopper_object
         )
         self.__program_state_edit = state_edit_module.StateEdit(
-            __ui_object, self.__data_object
+            self.__ui_object, self.__data_object
         )
         self.__program_state_new = state_new_module.StateNew(
-            __ui_object, self.__data_object
+            self.__ui_object, self.__data_object
         )
         self.__program_state_progress = state_progress_module.StateProgress(
-            __ui_object,
+            self.__ui_object,
             self.__hopper_object,
             self.__scale_object,
             __configuration["configure_max_waiting_time"],
         )
 
+        ################################################################################################################
+        # start the program
+        ################################################################################################################
+
         self.__program_state = self.__program_state_selection
         self.__program_is_running = True
         self.__program_loop()
 
+    ####################################################################################################################
+    # main program loop
+    ####################################################################################################################
+
     def __program_loop(self):
         while self.__program_is_running:
+
             __ui_cmd: dict = self.__program_state.call_ui()
+            # call_ui() returns dict{"cmd": str, "data": some_data}
 
             if __ui_cmd["cmd"] == "exit":
                 self.__data_object.close()
                 self.__hopper_object.close()
                 self.__scale_object.close()
                 self.__program_is_running = False
+
             elif __ui_cmd["cmd"] == "change_ui":
                 self.__change_program_state(__ui_cmd["data"])
+
             elif __ui_cmd["cmd"] == "progress":
                 if __ui_cmd["data"] == -1:
-                    # dispense drink is finished return to selection
+                    # progress of dispense drink is finished return to selection
                     self.__program_state = self.__program_state_selection
                 else:
                     self.__program_state.execute_command(__ui_cmd["data"])
+
             elif __ui_cmd["cmd"] == "edit":
                 self.__program_state_edit.execute_command(__ui_cmd["data"])
+
             elif __ui_cmd["cmd"] == "new":
                 self.__program_state_new.execute_command(__ui_cmd["data"])
 
