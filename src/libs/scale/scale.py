46cd0798aba3882617257be8c396728f15f54c5e
David P. Federl <david-peter.federl@stud.uni-due.de>
2024-01-22 16:45:05 +0100

    feat(scale): add tare function

--- src/libs/scale/scale.py
+++ src/libs/scale/scale.py
@@ -5,10 +5,16 @@ class Scale:
 
     __hardware = None
     __number_of_measurements: int
+    __base_value: int
 
     def __init__(self, hardware, number_of_measurements: int = 3):
-        __hardware = hardware
-        __number_of_measurements = number_of_measurements
+        self.__hardware = hardware
+        self.__number_of_measurements = number_of_measurements
+        self.__base_value = 0
+
+    def tare(self):
+        current = self.get_weight()
+        self.__base_value = current
 
     def get_weight(self) -> int:
         """
@@ -18,4 +24,5 @@ class Scale:
             return -1
         measurement = self.__hardware.get_weight(self.__number_of_measurements)
         measurement = int(round(measurement * 1000))  # convert from _mg_ to _g_
+        measurement = measurement + self.__base_value
         return measurement
