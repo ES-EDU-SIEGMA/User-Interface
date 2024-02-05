8cc185cd0211afb9823f6ad192c1152662622374
Kamil <77341120+kamilloo1@users.noreply.github.com>
2024-02-05 17:52:12 +0100

    refactor(data-drink)

--- src/libs/data/drink/drink.py
+++ src/libs/data/drink/drink.py
@@ -2,11 +2,16 @@ from src.libs.data.drink.ingredient import Ingredient
 
 
 class Drink:
-
-    __ID: int
+    __id_: int
     __ingredients: list[Ingredient]
 
-    def __init__(self, ID, ingredients):
-        self.__ID = ID
+    def __init__(self, id_, ingredients):
+        self.__id_ = id_
         self.__ingredients = ingredients
         self.__base_value = 0
+
+    def get_id(self) -> int:
+        """
+        :return: Drink-ID
+        """
+        return self.__id_
--- src/libs/data/drink/ingredient.py
+++ src/libs/data/drink/ingredient.py
@@ -1,5 +1,12 @@
 class Ingredient:
-    __ID: int
+    __id_: int
     name: str
     flow_speed: int
-    hopper_id: int
+    hopper_id: None
+
+    def __init__(self, id_, name, flow_speed):
+        self.__id_ = id_
+        self.name = name
+        self.flow_speed = flow_speed
+        self.__base_value = 0
+
