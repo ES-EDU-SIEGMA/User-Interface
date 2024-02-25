eb09538ea998a4fbaa6ecf34a150dbfad5886f76
Kamil <77341120+kamilloo1@users.noreply.github.com>
2024-02-25 21:04:19 +0100

    refactor(data): Ingredient Class is ready

--- src/tests/test_data/test_datatypes/test_ingredient.py
+++ src/tests/test_data/test_datatypes/test_ingredient.py
@@ -1,9 +1,23 @@
+from __future__ import annotations
+
 import unittest
 
+from libs.data.datatypes.ingredient import Ingredient
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestIngredient(unittest.TestCase):
+    __ingredient_id: int = 1
+    name: str = "Sprite"
+    flow_speed: int = 1
+    dispenser_id: int = 1
+
+    __ingredient: Ingredient = None
+
+    def setUp(self):
+        self.__ingredient = Ingredient(ingredient_id=self.__ingredient_id, name=self.name,
+                                       flow_speed=self.flow_speed, dispenser_id=self.dispenser_id)
+
+    def test_get_id_function(self):
+        self.assertEqual(1, self.__ingredient.get_id())
 
 
 if __name__ == '__main__':
