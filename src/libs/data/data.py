6261a0128c62facd181a53a911cb669a7568c13d
David P. Federl <david-peter.federl@federl.digital>
2024-02-27 13:36:52 +0100

    feat(data): test(data): implement data class

--- src/libs/data/data.py
+++ src/libs/data/data.py
@@ -5,33 +5,62 @@ from libs.data.datatypes.ingredient import Ingredient
 from libs.data.data_handler.IDatahandler import IDatahandler
 
 
+class DataException(Exception):
+    pass
+
+
 class Data:
     __datahandler: IDatahandler = None
     __ingredients: list[Ingredient] = None
     __drinks: list[Drink] = None
 
     def __init__(self, datahandler: IDatahandler):
-        # TODO: load ingredients from JSON
-        # TODO: load drinks from JSON
-        raise NotImplementedError()
+        self.__datahandler = datahandler
+        self.__ingredients = self.__datahandler.read_ingredients()
+        self.__drinks = self.__datahandler.read_drinks()
 
     def get_ingredients(self) -> list[Ingredient]:
+        """
+        :return: list of ingredients
+        """
         return self.__ingredients
 
     def add_ingredient(self, ingredient: Ingredient) -> None:
-        # TODO: implement (add to list & store in JSON)
-        raise NotImplementedError()
+        """
+        :param ingredient: ingredient to be added
+
+        :raises DataException: if ingredient already exists
+        """
+        if ingredient in self.__ingredients:
+            raise DataException("Ingredient exists already!")
+
+        self.__ingredients.append(ingredient)
+        self.__datahandler.write_ingredients(self.__ingredients)
 
     def update_ingredient(self, ingredient: Ingredient) -> None:
-        # TODO: implement (update list & store in JSON)
-        raise NotImplementedError()
+        """
+        :param ingredient: ingredient to be updated
+
+        :raises DataException: if ingredient does not exist
+        """
+        if ingredient not in self.__ingredients:
+            raise DataException("Ingredient does not exist!")
+
+        for index, element in enumerate(self.__ingredients):
+            if element.get_id() == ingredient.get_id():
+                self.__ingredients[index] = ingredient
+                return
 
     def get_drinks(self) -> list[Drink]:
+        """
+        return: list of drinks
+        """
         return self.__drinks
 
     def get_drink(self, drink_id: int) -> Drink | None:
         """
         :param drink_id: ID of drink to get data from
+
         :return: Drink or None if drink does not exist
         """
         for drink in self.__drinks:
@@ -40,9 +69,27 @@ class Data:
         return None
 
     def add_drink(self, drink: Drink) -> None:
-        # TODO: implement (add to list & store in JSON)
-        raise NotImplementedError()
+        """
+        :param drink: Drink to be added
+
+        :raises DataException: if drink already exists
+        """
+        if drink not in self.__drinks:
+            raise DataException("Drink exists already!")
+
+        self.__drinks.append(drink)
+        self.__datahandler.write_drinks(self.__drinks)
 
     def update_drink(self, drink: Drink) -> None:
-        # TODO: implement (update list & store in JSON)
-        raise NotImplementedError()
+        """
+        :param drink: Drink to be updated
+
+        :raises DataException: if drink does not exist
+        """
+        if drink not in self.__drinks:
+            raise DataException("Drink does not exist!")
+
+        for index, element in enumerate(self.__drinks):
+            if element.get_id() == drink.get_id():
+                self.__drinks[index] = drink
+                return
---
+++ src/tests/test_data/test_data/test_data.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import unittest
+
+
+class TestData(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_ingredients_for_empty_list(self):
+        pass
+
+    def test_get_ingredients_successful(self):
+        pass
+
+    def test_add_ingredient_raises_exception(self):
+        pass
+
+    def test_add_ingredient_successful(self):
+        pass
+
+    def test_update_ingredients_raises_exception(self):
+        pass
+
+    def test_update_ingredients_successful(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
